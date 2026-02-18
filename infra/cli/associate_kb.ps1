d5905069ca2fab716bd35403d69713c6ce0c8f13
Jordan Oberrath <oberrath.jordan@gmail.com>
2026-02-17 22:32:07 -0500

    Feature/pipeline (#19)
    * add the pipeline
    
    * fix esbuild permissions
    
    * bootstrap to strore terraform state
    
    * fixx bootstrap to use if exists, otherwise create
    
    * remove bootstrap now that resources exist
    
    * no region lambda
    
    * fix install awscli
    
    * Fix powershell scripts so Github actions doesn't use bash
    
    * fix the kb setup
    
    * use s3VectoresConfiguration
    
    * vectorBucketARN
    
    * have to manually create the s3  vector store
    
    * fix the kb policy
    
    * fix arn
    
    * fix associate
    
    * fix associate

--- infra/cli/associate_kb.ps1
+++ infra/cli/associate_kb.ps1
@@ -9,9 +9,14 @@ $AGENT_VERSION = aws bedrock-agent get-agent `
   --query "agent.agentVersion" `
   --output text
 
+# If AWS returns None, use DRAFT
+if (-not $AGENT_VERSION -or $AGENT_VERSION -eq "None") {
+    $AGENT_VERSION = "DRAFT"
+}
+
 Write-Host "Using Agent Version: $AGENT_VERSION"
 
-aws bedrock-agent associate-knowledge-base `
+aws bedrock-agent associate-agent-knowledge-base `
   --agent-id $AgentId `
   --agent-version $AGENT_VERSION `
   --knowledge-base-id $KbId
