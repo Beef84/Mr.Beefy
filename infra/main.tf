9e032efef8e0c4d6b69f8415d1502a6230af59a4
JordanOberrath <oberrath.jordan@gmail.com>
2026-02-18 17:49:26 -0500

    integration_subtype =  LambdaFunction

--- infra/main.tf
+++ infra/main.tf
@@ -260,6 +260,7 @@ resource "aws_apigatewayv2_api" "http_api" {
 resource "aws_apigatewayv2_integration" "lambda_integration" {
   api_id                 = aws_apigatewayv2_api.http_api.id
   integration_type       = "AWS_PROXY"
+  integration_subtype    = "LambdaFunction"
   integration_uri        = aws_lambda_function.api.invoke_arn
   integration_method     = "POST"
   payload_format_version = "2.0"
