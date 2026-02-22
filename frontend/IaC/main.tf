8875e6cf96b4e312e715b9da23050fe665775b43
JordanOberrath <oberrath.jordan@gmail.com>
2026-02-22 16:09:53 -0500

    front tf pmsw in cache

--- frontend/IaC/main.tf
+++ frontend/IaC/main.tf
@@ -184,6 +184,20 @@ resource "aws_cloudfront_cache_policy" "api" {
   default_ttl = 0
   max_ttl     = 0
   min_ttl     = 0
+
+  parameters_in_cache_key_and_forwarded_to_origin {
+    cookies_config {
+      cookie_behavior = "none"
+    }
+
+    headers_config {
+      header_behavior = "none"
+    }
+
+    query_strings_config {
+      query_string_behavior = "none"
+    }
+  }
 }
 
 resource "aws_cloudfront_origin_request_policy" "api" {
